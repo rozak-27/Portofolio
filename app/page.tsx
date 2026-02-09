1bf98f71be0107b0b45c5a14ec5780b8d6c99382
Rozak limbong <jakskuy6@gmail.com>
2026-02-09 23:10:05 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,107 @@
-import Image from "next/image";
+import { Github, Linkedin, Mail, Instagram} from "lucide-react";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="min-h-screen w-full bg-gradient-to-br from-slate-950 via-blue-950 to-slate-950 ">
+      {/*gif*/}
+         <img
+         src="/lero.gif"
+         alt="Lero Lero Cat"
+         className=" absolute w-143  pt-64 right-9.5"
+          />
+       {/* Navbar */}
+      <nav className="fixed top-6 left-1/2 -translate-x-1/2 z-50 pt-10" >
+        <div className="bg-gray-900/80 backdrop-blur-md border border-gray-800 rounded-full px-8 py-4 shadow-xl">
+          <ul className="flex items-center gap-15">
+            <li>
+              <Link href="/"
+ 
+                className="text-gray-300 hover:text-white transition-colors duration-200 text-sm font-medium"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link 
+                href="/projects" 
+                className="text-gray-300 hover:text-white transition-colors duration-200 text-sm font-medium"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link 
+                href="/writing" 
+                className="text-gray-300 hover:text-white transition-colors duration-200 text-sm font-medium"
+              >
+                Experience
+              </Link>
+            </li>
+            <li>
+              <Link 
+                href="/about" 
+                className="text-gray-300 hover:text-white transition-colors duration-200 text-sm font-medium"
+              >
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link 
+                href="/resume" 
+                className="text-gray-300 hover:text-white transition-colors duration-200 text-sm font-medium"
+              >
+                Resume
+              </Link>
+            </li>
+          </ul>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+      </nav>
+      
+      <div className="max-w-5xl left-[5%] pl-30 pt-54 ">
+        <div className="flex gap-[2%]  left ">
+          <a 
+            href="https://github.com/rozak-27" 
             target="_blank"
-            rel="noopener noreferrer"
+            className="w-14 h-14 flex items-center justify-center rounded-2xl bg-slate-800/50 border border-slate-700/50 hover:bg-slate-700/50 transition-all duration-200"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
+            <Github className="w-6 h-6 text-gray-300" />
           </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          
+          <a 
+            href="https://www.linkedin.com/in/rozak-limbong-181a35317/" 
             target="_blank"
-            rel="noopener noreferrer"
+            className="w-14 h-14 flex items-center justify-center rounded-2xl bg-slate-800/50 border border-slate-700/50 hover:bg-slate-700/50 transition-all duration-200"
           >
-            Documentation
+            <Linkedin className="w-6 h-6 text-gray-300" />
+          </a>
+          
+          <a 
+            href="https://www.instagram.com/jakkbng/" 
+            target="_blank"
+            className="w-14 h-14 flex items-center justify-center rounded-2xl bg-slate-800/50 border border-slate-700/50 hover:bg-slate-700/50 transition-all duration-200"
+          >
+            <Instagram className="w-6 h-6 text-gray-300" />
+          </a>
+          <a 
+            href="jakskuy6@gmail.com"
+            className="w-14 h-14 flex items-center justify-center rounded-2xl bg-slate-800/50 border border-slate-700/50 hover:bg-slate-700/50 transition-all duration-200"
+          >
+            <Mail className="w-6 h-6 text-gray-300" />
           </a>
         </div>
-      </main>
+        <div className="flex flex-col leading-[0.3]  ">
+          <div className="absolute bottom-[50%]  translate-x-10 left-[5%]">
+            <h1 className="text-[60px] font-bold text-white leading-tight ">
+              Hi, I'm Rozak Limbong.
+            </h1>
+             <p className="text-[24px] text-[#DBDBDB]  font-italy opacity-70" >
+              Information System Undergraduate
+            </p>
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
